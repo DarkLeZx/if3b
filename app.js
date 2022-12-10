1deb2bacd59842a28de8e64a570a214efcb483ea
DarkLeZx <depniel08@mhs.mdp.ac.id>
2022-12-10 11:15:27 +0700

    hahaha

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ const authRoutes = require('./routes/auth')
 // (8) app.use (mendaftarkan middleware baru ke Express)
 app.use('/mahasiswa', mahasiswaRoutes)
 app.use('/auth',authRoutes)
+app.get("/",(req, res) => {
+    res.send("KAMU CANTIKK")
+})
 
 // (3) koneksi ke database mongodb
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
